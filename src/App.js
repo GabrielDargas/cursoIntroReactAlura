975c4521c6bca30464577e181df32f7b714d1d3b
GabrielDargas <68109894+GabrielDargas@users.noreply.github.com>
2021-03-15 10:54:40 -0300

    Aula 05

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeNotas from "./components/ListaDeNotas";
 
 class App extends Component {
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+    this.setState(novoEstado);
+  }
 
-  render(){
+  constructor() {
+    super();
+    this.state = {
+      notas: []
+    };
+  }
+
+  render() {
     return (
       <div>
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </div>
-    )
+    );
   }
 }
 
--- src/components/FormularioCadastro.jsx
+++ src/components/FormularioCadastro.jsx
@@ -1,25 +1,41 @@
 import React, { Component } from "react";
 
 class FormularioCadastro extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.titulo = ""
+    this.texto= ""
   }
   
-  handleMudancaTitulo(evento) {
+  _handleMudancaTitulo(evento) {
+    evento.stopPropagation()
     this.titulo = evento.target.value
-    console.log(this.titulo)
+  }
+
+  _handleMudancaTexto(evento) {
+    evento.stopPropagation()
+    this.texto = evento.target.value
+  }
+
+  _criarNota(evento){
+    evento.preventDefault()
+    evento.stopPropagation()
+    this.props.criarNota(this.titulo, this.texto)
+    
   }
 
   render() {
     return (
-      <form>
+      <form
+      onSubmit={this._criarNota.bind(this)}>
         <input
           type="text"
           placeholder="Titulo"
-          onChange={this.handleMudancaTitulo.bind(this)}
+          onChange={this._handleMudancaTitulo.bind(this)}
         />
-        <textarea placeholder="Escreva sua nota" />
+        <textarea 
+        placeholder="Escreva sua nota" 
+        onChange={this._handleMudancaTexto.bind(this)}/>
         <button>Criar Nota</button>
       </form>
     );
